54f05afa2905b2b96fc1b85c33bab6a5b9b24c35
mehedimrm22 <mehedi.mrm@gmail.com>
2025-02-10 22:08:48 +0200

    input field

---
+++ src/components/Palindrome_input/Palindrome_input.tsx
@@ -0,0 +1,10 @@
+const Palindrome_input = () => {
+  return (
+    <div className="palindrome-input">
+      <input type="text" placeholder="Enter a word" />
+      <button>Check</button>
+    </div>
+  );
+};
+
+export default Palindrome_input;
